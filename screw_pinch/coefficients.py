928742b9be4e3a5941231592e3dbbfcbb08ca044
Cameron Kuchta <cameronkuchta97@yahoo.com>
2022-11-08 12:00:47 -0600

    Move plasma parameter calculations to new file

--- screw_pinch/coefficients.py
+++ screw_pinch/coefficients.py
@@ -1,113 +1,67 @@
-import numpy as np
+"""
+Get various terms/values for the screw pinch in the BRB.
+"""
+
 import matplotlib.pyplot as plt
 from scipy.misc import derivative as diff
-from scipy.optimize import newton
-import yaml
 from plot_parameters import get_config
-
-# Setting up the physical quatities that are constant
-config = get_config()
-
-b_helmholtz = config['experiment']['B_H_at_0']                        # The magnetic field generated due to the Helmholtz coil
-mu_0 = 4 * np.pi * 10**(-7)
-max_radius = config['domain']['r_end']                          # Radius of the brb
-max_current = config['coefficients']['max_current']                   # Maximum current value for the torroidal field coil
-
-r = np.linspace(config['domain']['r_start'], max_radius, config['coefficients']['r_points'])
-# i = config['coefficients']['current']
-dr = config['coefficients']['dr']                                # Differential step for differentiation done while calculating values for different fuctions
-bounds = [(config['domain']['r_start'], max_radius)]
-
-def b_tf(r, i):
-    """
-    Get the torroidal field at various r and i values
-
-    params:
-    r = radius values
-    i = current values
-    """
-    if isinstance(r, float):
-        if r==0:
-            return np.inf
-        else:
-            return (mu_0 * i)/(2 * np.pi * r * b_helmholtz)
-    else:
-        return np.where(r==0, np.inf, (mu_0 * i )/(2 * np.pi * r * b_helmholtz))
+from screw_pinch.plasma_parameters import b_tf, b_z, beta
 
 
-def beta(r):
-    """
-    Get the plasma confinement factor at various r values
-
-    params:
-    r = radius
-
-    """
-    # return (((1 + np.cos((np.pi * r)/max_radius)) * 0.02) + 0.01)
-    return (0.05 * np.exp((-r)/(max_radius**2)))
-
-def b_z(r):
-    """
-    Get the residual magnetic field after the interaction
-    between plasma and background magnetic field at various r values
-
-    params:
-    r = radius
-
-    """
-    # return ((b_helmholtz)/(np.sqrt(1 + beta(r))))
-    return ((1)/(np.sqrt(1 + beta(r))))
-
-# def p_0(r):
-#     """
-#     Get the density of the plasma at various r values
-
-#     params:
-#     r = radius values
-
-#     """
-#     return ((b_helmholtz**2 - b_z(r)**2)/(2 * mu_0))
-
-
-# Calculation for getting the coefficient of radial displacement vector(\xi\)
+# QUESTION: What are these commented out terms?
 def coeff_1(r, config):
+    """
+    Calculation for getting the coefficient of radial displacement vector(xi). `xi`
+
+    Returns
+    -------
+    SIMULATION
+    """
     i = config['coefficients']['current']
+    dr = config['coefficients']['dr']
 
     #term_1 = (-5/3) * b_helmholtz**2 + 2 * r * diff(b_tf, r, args=[i], dx=dr) * (2 * b_tf(r, i) + r * diff(b_tf, r, args=[i], dx=dr))
-    term_1 = -5 + 12 * r * b_tf(r, i) * diff(b_tf, r, args=[i], dx=dr)
+    term_1 = -5 + 12 * r * b_tf(r) * diff(b_tf, r, dx=dr)
 
     #term_2 = (-1/3) * b_z(r) * (b_z(r) - 2 * r * diff(b_z, r, dx=dr))
-    term_2 = 6 * r**2 * (diff(b_tf, r, args=[i], dx=dr))**2 - b_z(r)**2
+    term_2 = 6 * r**2 * (diff(b_tf, r, dx=dr))**2 - b_z(r)**2
 
     #term_3 = 2 * r**2 * b_tf(r, i) * diff(b_tf, r, n=2, args=[i], dx=dr)
-    term_3 = 2 * r * b_z(r) * diff(b_z, r, dx=dr) + 6 * r**2 * b_tf(r, i) * diff(b_tf, r, n=2, args=[i], dx=dr)
+    term_3 = 2 * r * b_z(r) * diff(b_z, r, dx=dr) + 6 * r**2 * b_tf(r) * diff(b_tf, r, n=2, dx=dr)
 
     return ((1/(r**2 * (1 - b_z(r)**2))) * (term_1 + term_2 + term_3))
 
-# Calculation for getting the coefficient of first order partial differential of the radial displacement vector(\xi\)
 def coeff_2(r, config):
-    i = config['coefficients']['current']
+    """
+    Calculation for getting the coefficient of first order partial differential of the radial displacement vector(xi). `d/dr xi`
+
+    Returns
+    -------
+    SIMULATION
+    """
+    dr = config['coefficients']['dr']
 
     #term_1 = (5/3) * b_helmholtz**2 + 4 * b_tf(r,i)**2 + 6 * r * b_tf(r,i) * diff(b_tf, r, args=[i], dx=dr)
-    term_1 = 5 + 12 * (b_tf(r, i))**2 + 18 * r * b_tf(r, i) * diff(b_tf, r, args=[i], dx=dr)
+    term_1 = 5 + 12 * (b_tf(r))**2 + 18 * r * b_tf(r) * diff(b_tf, r, dx=dr)
 
     #term_2 = (1/3) * b_z(r) * (b_z(r) + 2 * r * diff(b_z, r, dx=dr))
     term_2 = b_z(r)**2 + 2 * r * b_z(r) * diff(b_z, r, dx=dr)
     
     return ((1/(r * (1 - b_z(r)**2))) * (term_1 + term_2))
 
-# Calculation for getting the coefficient of second order partial differential of the radial displacement vector(\xi\)
 
 def coeff_3(r, config):
-    i = config['coefficients']['current']
+    """
+    Calculation for getting the coefficient of second order partial differential of the radial displacement vector(xi). `d^2 / dr^2 xi`
 
+    Returns
+    -------
+    SIMULATION
+    """
     #term_1 = (5/3) * b_helmholtz**2 + 2 * b_tf(r,i)**2 + (b_z(r)**2)/3
-    term_1  = 5 + 6 * (b_tf(r, i))**2 + b_z(r)**2
+    term_1  = 5 + 6 * (b_tf(r))**2 + b_z(r)**2
 
     return ((1/(1 - b_z(r)**2)) * term_1)
-
-
-
 
 def plot_coefficents(r, config):
     # Plotting the coefficients over range of radius values for a constant value of current
@@ -138,7 +92,4 @@ def plot_coefficents(r, config):
     plt.legend()
 
     plt.show()
-
-if __name__=="__main__":
-    plot_coefficents(r, config)
 
